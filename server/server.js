9e633271bd9a39b3bff0691de17fd711a084f585
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-10-13 15:15:00 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const portfoilioRoute = require("./routes/portfolioRotes");
 
 app.use(express.json());
 
-app.use("https://chubham.vercel.app/", portfoilioRoute);
+app.use("https://chubham.vercel.app", portfoilioRoute);
 
 
 const port = process.env.PORT || 5000
